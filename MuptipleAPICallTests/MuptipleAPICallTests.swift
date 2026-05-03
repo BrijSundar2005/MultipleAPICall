115f1e8a6edbc6fe4159042853a326288d087622
brijsundar2005 <brijsundar2005@gmail.com>
2026-05-03 19:11:22 +0530

    Initial Commit

---
+++ MuptipleAPICallTests/MuptipleAPICallTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuptipleAPICallTests.swift
+//  MuptipleAPICallTests
+//
+//  Created by Brij Sundar on 03/05/26.
+//
+
+import Testing
+@testable import MuptipleAPICall
+
+struct MuptipleAPICallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
