3a0e2458d30cd1890f9bbd6b0f3824a0fda94d5f
Brij Sundar <brijsundar2005@gmail.com>
2026-05-06 11:10:54 +0530

    add combine api call for unit test

--- MuptipleAPICallTests/CombineAPICallTest.swift
+++ MuptipleAPICallTests/CombineAPICallTest.swift
@@ -5,9 +5,28 @@
 //  Created by Brij Sundar on 05/05/26.
 //
 import XCTest
+import Combine
+
 @testable import MuptipleAPICall
 
 class CombineAPICallTest: XCTestCase {
+    var cancellables: Set<AnyCancellable> = []
     
+    func testLoadPostsSuccess() {
+        let mock = CombineMockTest()
+        let viewModel = CombinePipeLineViewModel()
+        
+        let expectation = XCTestExpectation(description: "Posts loaded")
+        
+        viewModel.$post
+            .dropFirst() // ignore initial empty value
+            .sink { posts in
+                XCTAssertEqual(posts.count, 1)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        viewModel.loadDataWithZip()
+        wait(for: [expectation], timeout: 2)
+    }
 }
 
